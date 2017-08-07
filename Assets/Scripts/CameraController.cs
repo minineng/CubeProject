28ad7a061e420792afe0431bdb2fe403cf07b3a5
Carlos Sanchis <minineng@gmail.com>
2017-08-07 20:16:09 +0200

    Camera Finished

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Vector3 lookAt;
+
+    private Vector3 initialPosition;
+    private Vector3 lastSavedPosition;
+
+    public float cameraRadius;
+    public float actualRadius;
+    public float cameraSpeed;
+    public float cameraHeight;
+    public float maxHeight;
+    public float minHeight;
+
+    public bool movement;
+
+    public float x;
+    public float z;
+
+    // Use this for initialization
+    void Start()
+    {
+        x = 0;
+        z = 0;
+        minHeight = 0;
+        maxHeight = 20;
+
+        lastSavedPosition = new Vector3(-1, -1, -1);
+
+        initialPosition = new Vector3(-1, -1, -1);
+
+        cameraHeight = transform.position.y;
+
+        lookAt = GetComponentInParent<LevelController>().getCameraLookingPoint(); ;
+
+
+    }
+
+    void Update()
+    {
+
+        if (cameraHeight < minHeight)
+            cameraHeight = minHeight;
+        else if (cameraHeight > maxHeight)
+            cameraHeight = maxHeight;
+
+        if (movement)
+            actualRadius = actualRadius + cameraSpeed;
+        else
+        {
+            float mouseInc = 0.5f;
+
+            if (Input.GetAxis("CameraMovement") > 0 && initialPosition == new Vector3(-1, -1, -1))
+            { //Saves the inital position
+                initialPosition = Input.mousePosition;
+                lastSavedPosition = initialPosition;
+            }
+
+            else if (Input.GetAxis("CameraMovement") > 0 && initialPosition != new Vector3(-1, -1, -1) && lastSavedPosition != Input.mousePosition)
+            {
+
+                float diff = (initialPosition.x - Input.mousePosition.x) / 100;
+
+                lastSavedPosition = Input.mousePosition;
+
+                print("Me muevo " + diff);
+
+                actualRadius = actualRadius + diff;
+
+            }
+
+            else if (Input.GetAxis("CameraMovement") > 0 && initialPosition != new Vector3(-1, -1, -1) && lastSavedPosition == Input.mousePosition)
+            {
+                initialPosition = Input.mousePosition;
+            }
+
+            else if (Input.GetAxis("CameraMovement") == 0 && initialPosition != new Vector3(-1, -1, -1))
+            {
+                initialPosition.Set(-1, -1, -1);
+            }
+
+
+        }
+
+        x = cameraRadius * Mathf.Cos(actualRadius * Mathf.Deg2Rad);
+        z = cameraRadius * Mathf.Sin(actualRadius * Mathf.Deg2Rad);
+
+        if (actualRadius > 360)
+            actualRadius = 0;
+
+        transform.position = new Vector3(x, cameraHeight, z);
+
+
+        transform.LookAt(lookAt);
+
+    }
+}
---
+++ Assets/Scripts/MoveButtonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveButtonController : MonoBehaviour {
+
+    public Sprite image;
+
+	// Use this for initialization
+	void Start () {
+        image = new Sprite();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
